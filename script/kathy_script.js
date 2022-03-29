d15af1587a5e5b37e43f6d29858705caf590e29c
LeKathy <71793868+LeKathy@users.noreply.github.com>
2022-03-29 10:04:05 -0400

    thishurtting

---
+++ script/kathy_script.js
@@ -0,0 +1,20 @@
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+
+var triggerEl = document.querySelector('#myTab a[href="#profile"]')
+bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
+
+var triggerFirstTabEl = document.querySelector('#myTab li:first-child a')
+bootstrap.Tab.getInstance(triggerFirstTabEl).show() // Select first tab
+
+var someListItemEl = document.querySelector('#someListItem')
+var tab = new bootstrap.Tab(someListItemEl)
+
+tab.show()
