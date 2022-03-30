18ccbb429b3dd6a7259afa7ed76e241a0de14f58
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2022-03-30 15:00:15 -0400

    Map Implementation

---
+++ script/map.js
@@ -0,0 +1,8 @@
+var LatLng = { lat: 42.3601, lng: -71.0589 };
+var mapOptions = {
+    center: LatLng,
+    zoom: 9,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions)
