47caf025787901e2bfa81249efe47a788ee63a9d
Tynan Hollyer <tynanmjhollyer@gmail.com>
2022-03-30 18:42:23 -0400

    last format fixes for google signing on index.html

--- script/tynan_script.js
+++ script/tynan_script.js
@@ -1,7 +1,7 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     document.getElementById("remove_signin").innerHTML = "<h5 class=\"my-auto mx-auto\">" + profile.getName() + "</h5>";
-    document.getElementById("add_name").innerHTML += "<img class=\"img-fluid ms-4 mx-auto\" style=\"max-width: 50px\" src='" + profile.getImageUrl() + "' alt='Profile Picture'>";
+    document.getElementById("add_name").innerHTML = "<img class=\"img-fluid ms-4 mx-auto rounded-circle\" style=\"max-width: 50px\" src='" + profile.getImageUrl() + "' alt='Profile Picture'>";
 }
 
 // sign out link
