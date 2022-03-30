3cfadc9d41be2187d2c162a4fbed4e760acda695
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-03-30 13:32:44 -0400

    more google fixes

--- script/tynan_script.js
+++ script/tynan_script.js
@@ -1,13 +1,8 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    document.getElementById("g-signin2").innerHTML = "<h1>" + profile.getName() + "</h1>";
+    document.getElementById("g-signin2").innerHTML += "<img src='" + profile.getImageUrl() + "' alt='Profile Picture'>";
 }
-
-document.getElementById("g-signin2").innerHTML = "<h1>" + googleUser.getBasicProfile().getName() + "</h1>";
-document.getElementById("g-signin2").innerHTML += "<img src='" + googleUser.getBasicProfile().getImageUrl() + "' alt='Profile Picture'>";
 
 // sign out link
 // <a href="#" onclick="signOut();">Sign out</a>
