c2fc0115df7e1fbf3ed135c7255012f314aca4ed
Tynan Hollyer <tynanmjhollyer@gmail.com>
2022-03-30 15:06:36 -0400

    quick formatting fix

--- script/tynan_script.js
+++ script/tynan_script.js
@@ -1,10 +1,8 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     document.getElementById("remove_signin").innerHTML = "<h5 class=\"my-auto mx-auto\">" + profile.getName() + "</h5>";
-    document.getElementById("remove_signin").innerHTML += "<img class=\"img-fluid ms-4 mx-auto\" style=\"max-width: 50px\" src='" + profile.getImageUrl() + "' alt='Profile Picture'>";
+    document.getElementById("add_name").innerHTML += "<img class=\"img-fluid ms-4 mx-auto\" style=\"max-width: 50px\" src='" + profile.getImageUrl() + "' alt='Profile Picture'>";
 }
-
-document.getElementById("add_name").innerHTML += "<img class=\"img-fluid ms-4 mx-auto\" style=\"max-width: 50px\" src='assets\\navbar\\profile_blank.webp' alt='Profile Picture'>";
 
 // sign out link
 // <a href="#" onclick="signOut();">Sign out</a>
