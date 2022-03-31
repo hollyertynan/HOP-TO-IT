965de9c2e0844dd1e98da19e0c007ffafb6d80c9
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-03-31 11:39:14 -0400

    quick test

--- script/tynan_script.js
+++ script/tynan_script.js
@@ -1,7 +1,18 @@
+var saveProfileImage;
+var saveProfileName;
+
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     document.getElementById("remove_signin").innerHTML = "<h5 class=\"my-auto mx-auto\">" + profile.getName() + "</h5>";
     document.getElementById("add_name").innerHTML = "<img class=\"img-fluid ms-4 mx-auto rounded-circle\" style=\"max-width: 50px\" src='" + profile.getImageUrl() + "' alt='Profile Picture'>";
+
+    saveProfileImage = profile.getImageUrl();
+    saveProfileName = profile.getName();
+}
+
+function onLoad() {
+  document.getElementById("remove_signin").innerHTML = "<h5 class=\"my-auto mx-auto\">" + saveProfileName + "</h5>";
+  document.getElementById("add_name").innerHTML = "<img class=\"img-fluid ms-4 mx-auto rounded-circle\" style=\"max-width: 50px\" src='" + saveProfileImage + "' alt='Profile Picture'>";
 }
 
 // sign out link
