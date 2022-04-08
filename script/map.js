8872245c16e5bc737776b18cd88f56ff0593655a
rvekeria999 <81255193+rvekeria999@users.noreply.github.com>
2022-04-08 10:30:35 -0400

    Module File Implementation, Sign-in Template Development

--- script/map.js
+++ script/map.js
@@ -1,8 +1,20 @@
-var LatLng = { lat: 42.3601, lng: -71.0589 };
+/*var LatLng = { lat: 42.3601, lng: -71.0589 };
 var mapOptions = {
     center: LatLng,
     zoom: 9,
     mapTypeId: google.maps.MapTypeId.ROADMAP
-};
+}
 
-var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions)
+var map = new google.maps.Map(document.getElementById('account_map'), mapOptions)
+*/
+
+export default class Map {
+    constructor(coordinates, tag_key) {
+        var map_options = {
+            center: coordinates,
+            zoom: 9,
+            mapTypeId: google.maps.MapTypeId.ROADMAP   
+        }
+        this.map = new google.maps.Map(document.getElementById(tag_key), map_options)
+    }
+}
